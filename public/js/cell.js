b9678e495bd150c26ba6f24f12b09afe500f06c9
Dmitry Umbras <freeudv@mail.ru>
2017-08-30 15:12:14 +0300

    Begin

--- public/js/cell.js
+++ public/js/cell.js
@@ -1,29 +1,48 @@
 class Cell {
-    constructor({ element, row, col, alive = false }) {
-        // опредлить свойства
+  constructor({ element, row, col, alive = false }) {
+    // опредлить свойства
+    this.element = element;
+    this.row = row;
+    this.col = col;
+    this._alive = alive;
+    // доинициализировать свойства в методе `init`
+    this.init();
+  }
 
-        // доинициализировать свойства в методе `init`
-        this.init();
+  get alive() {
+    // вернуть состояние клетки
+    return this._alive;
+  }
+
+  set alive(value) {
+    // обвновить состояние клетки
+    this._alive = value;
+
+    // если клетка жива, присвоить класс `alive`
+    // в противном случае убрать класс `alive`
+    if (this._alive) {
+      this.element.classList.add("alive");
+    } else {
+      this.element.classList.remove("alive");
+    }
+  }
+
+  init() {
+    // элементу присвоить класс `cell`
+    // если клетка жива присвоить ей класс `alive`
+    // подписаться на событие `click` у элемента
+    this.element.classList.add("cell");
+
+    if (this._alive) {
+      this.element.classList.add("alive");
     }
 
-    get alive() {
-        // вернуть состояние клетки
-    }
+    this.element.addEventListener("click", this.handleClick.bind(this));
+  }
 
-    set alive(value) {
-        // обвновить состояние клетки
-        
-        // если клетка жива, присвоить класс `alive`
-        // в противном случае убрать класс `alive`
-    }
-
-    init() {
-        // элементу присвоить класс `cell`
-        // если клетка жива присвоить ей класс `alive`
-        // подписаться на событие `click` у элемента
-    }
-
-    handleClick(event) {
-        // при нажатии на клетку изменить ее состояние
-    }
+  handleClick(event) {
+    // при нажатии на клетку изменить ее состояние
+    this._alive = !this._alive;
+    this.element.classList.toggle("alive");
+  }
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 new Game({
-    element: document.querySelector('#game'),
-    grid: new Grid({
-        element: document.querySelector('#grid'),
-        size: 36
-    })
+  element: document.querySelector("#game"),
+  grid: new Grid({
+    element: document.querySelector("#grid"),
+    size: 10
+    //size: 36
+  })
 });
